8b7f61a1fe4e4c3e6c3e1890ca612f19f4f18848
KangarooPotato <w0770897@myscc.ca>
2022-03-29 07:22:39 -0400

    admin and Customer

--- JunsBookStore/Areas/Customer/Controllers/HomeController.cs
+++ JunsBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace JunsBookStore.Controllers
+namespace JunsBookStore.Area.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- JunsBookStore/Startup.cs
+++ JunsBookStore/Startup.cs
@@ -64,7 +64,7 @@ namespace JunsBookStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
